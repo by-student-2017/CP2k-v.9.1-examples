19e61269625ba796b74d6ff9a2f991bc1a20d0c9
by-student-2017 <studentsctest@gmail.com>
2024-08-10 17:05:48 +0900

    ubuntu 22.04 LTS

--- NEB/2016_CP2K_Summer_School_2016_KCL/Exercise1_NEB_EX2/run_neb_GPW.sh
+++ NEB/2016_CP2K_Summer_School_2016_KCL/Exercise1_NEB_EX2/run_neb_GPW.sh
@@ -14,4 +14,4 @@ for ((i=1;i<=${ndata};i++)); do
   tail -n $((2+2936)) "NEB-pos-Replica_nr_${a}-1.xyz" >> neb_aba_14r.xyz
 done
 
-awk 'BEGIN{E0=-999999.99} /E =/ {i=i+1; printf "No.%s, %16.8f [Ha], Line:%d \n", i, $6, NR; if($6>E0){No=i; E0=$6; Line=NR}} END{printf "TS: No. %d, %16.8f [Ha], Line:%d \n", No, E0, Line}' neb_aba_14r.xyz
+awk 'BEGIN{E0=-999999.99} /E =/ {i=i+1; printf "No.%s, %16.8f [Ha], Line:%d \n", i, $6, NR; if($6>E0){No=i; E0=$6; Line=NR}} END{printf "Emax: No. %d, %16.8f [Ha], Line:%d \n", No, E0, Line}' neb_aba_14r.xyz
