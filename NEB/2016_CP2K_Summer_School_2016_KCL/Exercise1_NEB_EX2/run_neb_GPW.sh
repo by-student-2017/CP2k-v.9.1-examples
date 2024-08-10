f94a1116a2d0cf37c67fdb620fdddf9466e3e8b1
by-student-2017 <studentsctest@gmail.com>
2024-08-10 15:11:59 +0900

    ubuntu 22.04 LTS

--- NEB/2016_CP2K_Summer_School_2016_KCL/Exercise1_NEB_EX2/run_neb_GPW.sh
+++ NEB/2016_CP2K_Summer_School_2016_KCL/Exercise1_NEB_EX2/run_neb_GPW.sh
@@ -6,3 +6,12 @@ NCPUs=4
 export OMP_NUM_THREADS=1
 
 mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i neb_GPW.inp | tee cp2k_neb_GPW.out
+
+ndata=14
+
+for ((i=1;i<=${ndata};i++)); do
+  a=`echo $i | awk '{printf("%02d",$1)}'`
+  tail -n $((2+2936)) "NEB-pos-Replica_nr_${i}-1.xyz" >> neb_aba_14r.xyz
+done
+
+awk 'BEGIN{E0=-999999.99} /E =/ {i=i+1; printf "No.%s, %16.8f [Ha], Line:%d \n", i, $6, NR; if($6>E0){No=i; E0=$6; Line=NR}} END{printf "TS: No. %d, %16.8f [Ha], Line:%d \n", No, E0, Line}' neb_aba_14r.xyz
