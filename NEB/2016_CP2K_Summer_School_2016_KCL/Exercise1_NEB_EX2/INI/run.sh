17a3de2276593526fabd2a3b5642babd8ce93d3f
by-student-2017 <studentsctest@gmail.com>
2024-08-10 14:50:46 +0900

    ubuntu 22.04 LTS

---
+++ NEB/2016_CP2K_Summer_School_2016_KCL/Exercise1_NEB_EX2/INI/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp2k_adress="/usr/bin"
+NCPUs=4
+
+export OMP_NUM_THREADS=1
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i geo.inp | tee cp2k_geo.out
+
+cp INI-pos-1.xyz s1_ini_eq.xyz
--- NEB/README.md
+++ NEB/README.md
@@ -4,4 +4,5 @@
 ## Nudged Elastic Band
 - 2014_Molecular_and_Materials_Modelling_ETHZ
 - 2015_Molecular_and_Materials_Modelling_ETHZ
+- 2016_Molecular_and_Materials_Modelling_ETHZ
 
