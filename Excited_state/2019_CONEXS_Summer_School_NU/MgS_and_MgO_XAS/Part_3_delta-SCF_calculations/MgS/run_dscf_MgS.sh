fa7f74da68ea0ddeb2033fd4c9b856d2a389fbe7
by-student-2017 <studentsctest@gmail.com>
2024-08-12 14:48:47 +0900

    ubuntu 22.04 LTS

--- Excited_state/2019_CONEXS_Summer_School_NU/MgS_and_MgO_XAS/Part_3_delta-SCF_calculations/MgS/run_dscf_MgS.sh
+++ Excited_state/2019_CONEXS_Summer_School_NU/MgS_and_MgO_XAS/Part_3_delta-SCF_calculations/MgS/run_dscf_MgS.sh
@@ -7,6 +7,8 @@ export OMP_NUM_THREADS=1
 
 mpirun -np ${NCPUs} ${cp2k_adress}/cp2k MgS_dscf.inp | tee MgS_dscf.out
 
+grep -n "excited atom" *.out
+
 echo "in the output file. The energy is given in Hartree, and to convert it to electron volts multiply the value by 27.211. This is the energy of the first transition, and you can use this value to rigidly shift your absorption spectrum."
 
 echo "1st: at1 = X = S"
