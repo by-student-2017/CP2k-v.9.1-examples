702ca42e535ea1668684778c0df5a8ab842bc9af
by-student-2017 <studentsctest@gmail.com>
2024-08-12 15:51:04 +0900

    ubuntu 22.04 LTS

--- Diffusion/2014_Molecular_and_Materials_Modelling_ETHZ/H2O_MD/run.sh
+++ Diffusion/2014_Molecular_and_Materials_Modelling_ETHZ/H2O_MD/run.sh
@@ -7,10 +7,8 @@ export OMP_NUM_THREADS=1
 
 mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i H2O-256.inp | tee H2O-256.out
 
-gfortran ./../LIB_TOOLS/msd.f90 -o ./../LIB_TOOLS/msd.x  # compile msd.x executable
-
-./../LIB_TOOLS/msd.x < msd.in # check input file 'msd.in' before you run!
-
 chmod +x get_t_sigma.sh
+./get_t_sigma.sh H2O-256.out
 
-./get_t_sigma.sh msd-64.dat
+gfortran ./../LIB_TOOLS/msd.f90 -o ./../LIB_TOOLS/msd.x  # compile msd.x executable
+./../LIB_TOOLS/msd.x < msd.in # check input file 'msd.in' before you run!
