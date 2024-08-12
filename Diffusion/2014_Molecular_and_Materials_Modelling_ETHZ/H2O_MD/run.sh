c790b792d76f86309356fe6b0bc2e8663c6f7d42
by-student-2017 <studentsctest@gmail.com>
2024-08-12 17:01:19 +0900

    ubuntu 22.04 LTS

--- Diffusion/2014_Molecular_and_Materials_Modelling_ETHZ/H2O_MD/run.sh
+++ Diffusion/2014_Molecular_and_Materials_Modelling_ETHZ/H2O_MD/run.sh
@@ -12,3 +12,5 @@ bash ./get_t_sigma.sh run-H2O-256-pos-1.xyz
 
 gfortran ./../LIB_TOOLS/msd.f90 -o ./../LIB_TOOLS/msd.x  # compile msd.x executable
 ./../LIB_TOOLS/msd.x < msd.in # check input file 'msd.in' before you run!
+
+echo "plot 'msd-256.dat' using 1:2 with l; set xlabel 'Time / fs' ; set ylabel 'Mean squared displacement / Angstrom^2' ; pause mouse" |  gnuplot
