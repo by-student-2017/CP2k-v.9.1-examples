1d82143eeca176cd1325b0c70ffe445f013cd99a
by-student-2017 <studentsctest@gmail.com>
2024-08-14 13:52:10 +0900

    ubuntu 22.04 LTS

---
+++ VMD_charmm/2017_Statistical_Mechanics_and_Molecular_Simulations_UZH/Task_2_Perform_constrained_MD_simulations/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp2k_adress="/usr/bin"
+NCPUs=4
+
+export OMP_NUM_THREADS=1
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_1536.inp | tee md_1536.out
+
+grep Shake deca_ala_md_1536.LagrangeMultLog | awk '{c++ ; s=s+$4}END{print s/c}'
