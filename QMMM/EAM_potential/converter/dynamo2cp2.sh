12c202823df7e411f2e04c32ce785b46648a5715
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-01 16:06:04 +0900

    Add files via upload

--- QMMM/EAM_potential/converter/dynamo2cp2.sh
+++ QMMM/EAM_potential/converter/dynamo2cp2.sh
@@ -64,7 +64,7 @@ for element in "${array_elem[@]}"; do
     #
     echo "Linear Interpolation version"
     awk -v Nrho=${Nrho} -v Nr=${Nr} -v drho=${drho} 'BEGIN{
-      #printf "%24.16e %24.16e \n",0.0,0.0
+      #printf "%24.16e  %24.16e \n",0.0,0.0
       o1=0.0; n0=0.0; n1=0.0; i=1 ; Ls=1
     }{
       if(NR<=Nr){
@@ -73,17 +73,17 @@ for element in "${array_elem[@]}"; do
       if(2<NR && NR<=Nrho){
         if(3==NR){
           dFrho=(n1 - n0)/drho
-          printf "%24.16e %24.16e \n",n0-dFrho*drho,dFrho
+          printf "%24.16e  %24.16e \n",n0-dFrho*drho,dFrho
         }
         dFrho=(n1 - o1)/(2.0*drho)
-        printf "%24.16e %24.16e \n",n0,dFrho
+        printf "%24.16e  %24.16e \n",n0,dFrho
       }
     }END{
       Frho = n1
       #dFrho=(n1 - o1)/(2.0*drho)
       for (i = Nrho; i <= Nr; i++){
         Frho = Frho + dFrho*drho
-        printf "%24.16e %24.16e \n",Frho,dFrho
+        printf "%24.16e  %24.16e \n",Frho,dFrho
       }
     }' Frho_Nrho_${element}.txt > Frho_Nr_${element}.txt
     #
@@ -92,7 +92,7 @@ for element in "${array_elem[@]}"; do
     #
     Ls=$((${Ls} + ${Nrho}))
     awk -v Ls=${Ls} -v Nr=${Nr} -v dr=${dr} 'BEGIN{
-      #printf "%24.16e %24.16e \n",0.0,0.0
+      #printf "%24.16e  %24.16e \n",0.0,0.0
       o1=0.0; n0=0.0; n1=0.0
     }{
       if(Ls<NR && NR<=Ls+Nr){
@@ -101,14 +101,14 @@ for element in "${array_elem[@]}"; do
       if(Ls+2<NR && NR<=Ls+Nr){
         if(Ls+3==NR){
           drho=(n1 - n0)/dr
-          printf "%24.16e %24.16e \n",n0-drho*dr,drho
+          printf "%24.16e  %24.16e \n",n0-drho*dr,drho
         }
         drho=(n1 - o1)/(2.0*dr)
-        printf "%24.16e %24.16e \n",n0,drho
+        printf "%24.16e  %24.16e \n",n0,drho
       }
     }END{
       drho=(n1 - n0)/dr
-      printf "%24.16e %24.16e \n",n1,drho
+      printf "%24.16e  %24.16e \n",n1,drho
     }' ${filename} > rho_Nr_${element}.txt
     #
     if [ "${element}" == "${array_elem[-1]}" ]; then
@@ -136,7 +136,7 @@ for ((i=1; i<=${array_elem[0]}; i++)); do
     echo "(${i}, ${j}) = (${array_elem[${i}]},${array_elem[${j}]}) , pair potential phi(r) (Nr values)"
     Ls=$((${Ls} + ${Nr}))
     awk -v Ls=${Ls} -v Nr=${Nr} -v dr=${dr} 'BEGIN{
-      #printf "%24.16e %24.16e \n",0.0,0.0
+      #printf "%24.16e  %24.16e \n",0.0,0.0
       o1=0.0; n0=0.0; n1=0.0; i=1
     }{
       if(Ls+1==NR){
@@ -148,14 +148,14 @@ for ((i=1; i<=${array_elem[0]}; i++)); do
       if(Ls+2<NR && NR<=Ls+Nr){
         if(Ls+3==NR){
           dphi = (n1 - n0)/dr
-          printf "%24.16e %24.16e \n",n0-dphi*dr,dphi
+          printf "%24.16e  %24.16e \n",n0-dphi*dr,dphi
         }
         dphi = (n1 - o1)/(2.0*dr)
-        printf "%24.16e %24.16e \n",n0,dphi
+        printf "%24.16e  %24.16e \n",n0,dphi
       }
     }END{
       dphi=(n1-n0)/dr
-      printf "%24.16e %24.16e \n",n1,dphi
+      printf "%24.16e  %24.16e \n",n1,dphi
     }' ${filename} > phi_Nr_${array_elem[${i}]}_${array_elem[${j}]}.txt
     #
     if [ "${i}" -ne "${j}" ]; then
@@ -176,7 +176,7 @@ for ((i=1; i<=${array_elem[0]}; i++)); do
     output="./results/${array_elem[${i}]}-${array_elem[${j}]}.eam.alloy"
     echo "title" > ${output}
     echo "${natm[${i}]}  ${mass[${i}]}  ${latt[${i}]}" >> ${output}
-    echo "${dr}  ${drho}  ${cutoff}  ${Nr}" >> ${output}
+    echo "${dr}  ${drho}  ${cutoff}  ${Nr}" | awk '{printf "%24.16e  %24.16e  %24.16e  %6d\n",$1,$2,$3,$4}' >> ${output}
     paste -d ' ' Frho_Nr_${array_elem[${i}]}.txt Frho_Nr_${array_elem[${j}]}.txt >> ${output}
     paste -d ' '  rho_Nr_${array_elem[${i}]}.txt  rho_Nr_${array_elem[${j}]}.txt >> ${output}
     paste -d ' '  phi_Nr_${array_elem[${i}]}_${array_elem[${i}]}.txt phi_Nr_${array_elem[${i}]}_${array_elem[${j}]}.txt >> ${output}
--- QMMM/EAM_potential/converter/fs2cp2.sh
+++ QMMM/EAM_potential/converter/fs2cp2.sh
@@ -79,7 +79,7 @@ for element in "${array_elem[@]}"; do
     #
     echo "Linear Interpolation version"
     awk -v Nrho=${Nrho} -v Nr=${Nr} -v drho=${drho} 'BEGIN{
-      #printf "%24.16e %24.16e \n",0.0,0.0
+      #printf "%24.16e  %24.16e \n",0.0,0.0
       o1=0.0; n0=0.0; n1=0.0; i=1
     }{
       if(NR<=Nr){
@@ -88,17 +88,17 @@ for element in "${array_elem[@]}"; do
       if(2<NR && NR<=Nrho){
         if(3==NR){
           dFrho=(n1 - n0)/drho
-          printf "%24.16e %24.16e \n",n1-dFrho*drho,dFrho
+          printf "%24.16e  %24.16e \n",n1-dFrho*drho,dFrho
         }
         dFrho=(n1 - o1)/(2.0*drho)
-        printf "%24.16e %24.16e \n",n0,dFrho
+        printf "%24.16e  %24.16e \n",n0,dFrho
       }
     }END{
       Frho = n1
       #dFrho=(n1 - o1)/(2.0*drho)
       for (i = Nrho; i <= Nr; i++){
         Frho = Frho + dFrho*drho
-        printf "%24.16e %24.16e \n",Frho,dFrho
+        printf "%24.16e  %24.16e \n",Frho,dFrho
       }
     }' Frho_Nrho_${element}.txt > Frho_Nr_${element}.txt
     #
@@ -114,7 +114,7 @@ for element in "${array_elem[@]}"; do
       #
       Ls=$((${Ls} + ${Nrho}))
       awk -v Ls=${Ls} -v Nr=${Nr} -v dr=${dr} 'BEGIN{
-        #printf "%24.16e %24.16e \n",0.0,0.0
+        #printf "%24.16e  %24.16e \n",0.0,0.0
         o1=0.0; n0=0.0; n1=0.0
       }{
         if(Ls<NR && NR<=Ls+Nr){
@@ -123,14 +123,14 @@ for element in "${array_elem[@]}"; do
         if(Ls+2<NR && NR<=Ls+Nr){
           if(Ls+3==NR){
             drho=(n1 - n0)/dr
-            printf "%24.16e %24.16e \n",n0-drho*dr,drho
+            printf "%24.16e  %24.16e \n",n0-drho*dr,drho
           }
           drho=(n1 - o1)/(2.0*dr)
-          printf "%24.16e %24.16e \n",n0,drho
+          printf "%24.16e  %24.16e \n",n0,drho
         }
       }END{
         drho=(n1 - n0)/dr
-        printf "%24.16e %24.16e \n",n1,drho
+        printf "%24.16e  %24.16e \n",n1,drho
       }' ${filename} > rho_Nr_${element}_${element_beta}.txt
       #
       j=$((${j} + 1))
@@ -174,14 +174,14 @@ for ((i=1; i<=${array_elem[0]}; i++)); do
       if(Ls+2<NR && NR<=Ls+Nr){
         if(Ls+3==NR){
           dphi = (n1 - n0)/dr
-          printf "%24.16e %24.16e \n",n0-dphi*dr,dphi
+          printf "%24.16e  %24.16e \n",n0-dphi*dr,dphi
         }
         dphi = (n1 - o1)/(2.0*dr)
-        printf "%24.16e %24.16e \n",n0,dphi
+        printf "%24.16e  %24.16e \n",n0,dphi
       }
     }END{
       dphi=(n1-n0)/dr
-      printf "%24.16e %24.16e \n",n1,dphi
+      printf "%24.16e  %24.16e \n",n1,dphi
     }' ${filename} > phi_Nr_${array_elem[${i}]}_${array_elem[${j}]}.txt
     #
     if [ "${i}" -ne "${j}" ]; then
@@ -202,7 +202,7 @@ for ((i=1; i<=${array_elem[0]}; i++)); do
     output="./results/${array_elem[${i}]}-${array_elem[${j}]}.eam.fs"
     echo "title" > ${output}
     echo "${natm[${i}]}  ${mass[${i}]}  ${latt[${i}]}" >> ${output}
-    echo "${dr}  ${drho}  ${cutoff}  ${Nr}" >> ${output}
+    echo "${dr}  ${drho}  ${cutoff}  ${Nr}" | awk '{printf "%24.16e  %24.16e  %24.16e  %6d\n",$1,$2,$3,$4}' >> ${output}
     paste -d ' ' Frho_Nr_${array_elem[${i}]}.txt Frho_Nr_${array_elem[${j}]}.txt >> ${output}
     paste -d ' '  rho_Nr_${array_elem[${i}]}_${array_elem[${i}]}.txt rho_Nr_${array_elem[${i}]}_${array_elem[${j}]}.txt >> ${output}
     paste -d ' '  phi_Nr_${array_elem[${i}]}_${array_elem[${i}]}.txt phi_Nr_${array_elem[${i}]}_${array_elem[${j}]}.txt >> ${output}
