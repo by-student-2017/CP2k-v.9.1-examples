1c9064ff680a7caced4695df7cc4abbf37b25255
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-07 18:13:44 +0900

    Add files via upload

---
+++ Infrared_spectroscopy/2018_Molecular_and_Materials_Modelling_ETHZ/Exercise_7/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cp2k_adress="/usr/bin"
+NCPUs=8
+
+export OMP_NUM_THREADS=1
+
+#----------------------------------------------------------------------
+
+echo "1. Task: Computing vibrational spectra for methanol and benzene"
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i optmet.inp | tee optmet.out
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i vibmet.inp | tee vibmet.out
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i optc6h6.inp | tee optc6h6.out
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i vibc6h6.inp | tee vibc6h6.out
+
+#----------------------------------------------------------------------
+
+#gfortran dipole_correlation.f90 -o dipole_gf.x
+
+#----------------------------------------------------------------------
+
+echo "2. Task: Computing vibrational spectra using DFTB molecular dynamics"
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i mdmet.inp | tee mdmet.out
+cp dipole_met.in dipole.in
+./dipole.x < dipole.in
+echo "plot “file” u 1:2 w l" | gnuplot -p -
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i mdc6h6.inp | tee mdc6h6.out
+cp dipole_c6h6.in dipole.in
+./dipole.x < dipole.in
+echo "plot “file” u 1:2 w l" | gnuplot -p -
+
+# https://www.cfa.harvard.edu/hitran/vibrational.html
+#----------------------------------------------------------------------
---
+++ Infrared_spectroscopy/README.md
@@ -0,0 +1,12 @@
+# Groups with the same content
+
+
+## Infrared spectroscopy (IR) with cp2k
+- 2018_Molecular_and_Materials_Modelling_ETHZ
+
+
+## Infrared spectroscopy with MD
+- 2014_Molecular_and_Materials_Modelling_ETHZ
+- 2015_Molecular_and_Materials_Modelling_ETHZ
+- 2016_Molecular_and_Materials_Modelling_ETHZ
+
