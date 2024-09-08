c0ffb13fbe1f7c66d2f34d8efd76a231c2b56732
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-08 14:22:02 +0900

    Add files via upload

--- Adsoprtion/2018_Molecular_and_Materials_Modelling_ETHZ/run.sh
+++ Adsoprtion/2018_Molecular_and_Materials_Modelling_ETHZ/run.sh
@@ -5,4 +5,8 @@ NCPUs=8
 
 export OMP_NUM_THREADS=1
 
-mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i S_M.ene.inp | tee S_M.ene.out
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i S_M.inp | tee S_M.out
+
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i S_M.ene.inp | tee S_M.ene.out
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i S_M.MD.inp | tee S_M.MD.out
