bfc2691c4f6b8072d56b3d74ec4fdfde6e674b28
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-09 17:41:50 +0900

    Add files via upload

---
+++ QMMM/2016_Multiscale_modelling/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp2k_adress="/usr/bin"
+NCPUs=4
+
+export OMP_NUM_THREADS=1
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i input_GPW.inp | tee input_GPW.out
+
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i input_PM6.inp | tee input_PM6.out
