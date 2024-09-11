eea68318295d9cdce5db1fe1252593a1498cfe03
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-11 17:06:57 +0900

    Add files via upload

--- QMMM/2014_Molecular_Simulations_UZH_Electric-field/2.4-CHP-CU111-GEOOPT/run.sh
+++ QMMM/2014_Molecular_Simulations_UZH_Electric-field/2.4-CHP-CU111-GEOOPT/run.sh
@@ -5,4 +5,6 @@ NCPUs=4
 
 export OMP_NUM_THREADS=1
 
-mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i geo.in | tee geo.out
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i geo.in | tee geo.out
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md.in | tee md.out
