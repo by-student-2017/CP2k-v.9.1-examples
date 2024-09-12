3e5b3624ea0e6c7ffcad7a50af59c4d8dfaaf3f1
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-12 13:00:38 +0900

    Add files via upload

--- QMMM/2014_Molecular_Simulations_UZH_Electric-field/2.4-CHP-CU111-MD-DFTB/run.sh
+++ QMMM/2014_Molecular_Simulations_UZH_Electric-field/2.4-CHP-CU111-MD-DFTB/run.sh
@@ -5,4 +5,10 @@ NCPUs=8
 
 export OMP_NUM_THREADS=1
 
-mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb.in | tee md_dftb.out
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i geo.in | tee geo.out
+
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md.in | tee md.out
+
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_FCC111.in | tee md_dftb_FCC111.out
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_BCC110.in | tee md_dftb_BCC110.out
