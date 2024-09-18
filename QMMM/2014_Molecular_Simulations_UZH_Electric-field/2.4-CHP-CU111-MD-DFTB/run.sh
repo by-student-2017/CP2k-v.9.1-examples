829399afc095a53d60b57ef129e08c18f580eae5
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-09-18 15:19:02 +0900

    Add files via upload

--- QMMM/2014_Molecular_Simulations_UZH_Electric-field/2.4-CHP-CU111-MD-DFTB/run.sh
+++ QMMM/2014_Molecular_Simulations_UZH_Electric-field/2.4-CHP-CU111-MD-DFTB/run.sh
@@ -5,12 +5,12 @@ NCPUs=8
 
 export OMP_NUM_THREADS=1
 
-#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i geo.in | tee geo.out
-
-#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md.in | tee md.out
-
 #mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_FCC111.in | tee md_dftb_FCC111.out
 
 #mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_BCC110.in | tee md_dftb_BCC110.out
 
-mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_HCP0001.in | tee md_dftb_HCP0001.out
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_HCP0001.in | tee md_dftb_HCP0001.out
+
+#mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_gra0001_tersoff.in | tee md_dftb_gra0001_tersoff.out
+
+mpirun -np ${NCPUs} ${cp2k_adress}/cp2k -i md_dftb_dia100_tersoff.in | tee md_dftb_dia100_tersoff.out
